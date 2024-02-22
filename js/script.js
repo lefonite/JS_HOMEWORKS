1233e05f1cf2eb16da31ce4ffe132dfbc28d1bb0
le-fonite <lefonite@gmail.com>
2024-02-23 01:37:00 +0200

    hw_7 completed

--- js/script.js
+++ js/script.js
@@ -1,2 +1,23 @@
 'use strict';
 
+// Task #1
+let name = 'John';
+let admin = name;
+alert(admin);
+
+// or?
+// let admin;
+// admin = name;
+
+// Task #2
+const planetName = 'Земля';
+const userName = 'Имя пользователя';
+
+// or?
+// const namePlanet = 'Земля';
+// let userName = 'Имя пользователя';
+
+// Task #3
+let result = '10' + 10;
+console.log(result);
+console.log(typeof result);
