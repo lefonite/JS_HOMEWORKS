6b2573d995f290007051086f26ada50a6d108b1d
le-fonite <lefonite@gmail.com>
2024-02-23 01:07:54 +0200

    hw_5 completed

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
 'use strict';
 
+let str1 = prompt('First num');
+let str2 = prompt('Second num');
+let str3 = prompt('Last num');
+
+alert(str1+ ' ' +str2+ ' ' +str3) ;
+
+
+// or
+// alert(`${str1} ${str2} ${str3}`);
