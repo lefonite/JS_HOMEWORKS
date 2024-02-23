bc50f7edd5ad10557c0873cdfd9ca8821710b3e7
le-fonite <lefonite@gmail.com>
2024-02-23 12:54:06 +0200

    hw_6 completed

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 'use strict';
-
-let str1 = prompt('First num');
-let str2 = prompt('Second num');
-let str3 = prompt('Last num');
+// #1
+const str1 = prompt('First num');
+const str2 = prompt('Second num');
+const str3 = prompt('Last num');
 
 alert(str1+ ' ' +str2+ ' ' +str3) ;
 
-
-// or
-// alert(`${str1} ${str2} ${str3}`);
+//#2
+const number = prompt('Введите пятизначное число:');
+alert(number.split('').join(' '));
