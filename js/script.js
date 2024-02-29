26ae90970b6686f1d15987e5451fc01090428bf5
le-fonite <lefonite@gmail.com>
2024-02-29 21:26:36 +0200

    hw_8 edit (const)

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 'use strict';
 
-let age = prompt ('Сколько Вам лет?');
-let result = parseInt(age) + 10;
+const age = prompt ('Сколько Вам лет?');
+const result = parseInt(age) + 10;
 alert('Через 10 лет, Вам будет ' + result + ' лет ✨');
