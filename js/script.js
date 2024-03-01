0d22deb3c1205ee479e7edb6b915f693ac7bf306
le-fonite <lefonite@gmail.com>
2024-03-01 12:30:56 +0200

    hw_9 completed

--- js/script.js
+++ js/script.js
@@ -1,2 +1,39 @@
 'use strict';
 
+// const temp = +prompt('Введите температуру:');
+// const type = prompt('Перевести в градусы Цельсия - C, c или Фаренгейты - F, f?');
+// let result;
+//
+// if (!isNaN(temp)) {
+//     if (type.toLowerCase() === 'f') {
+//         result = 'Результат: ' + ((temp - 32) * 5/9) + '°C';
+//     } else if (type.toLowerCase() === 'c') {
+//         result = 'Результат: ' + ((temp * 9/5) + 32) + '°F';
+//     } else {
+//         result = 'Ошибка! Введите "C" или "F" для указания типа температуры.';
+//     }
+// } else {
+//     result = 'Ошибка! Введите число вначале.';
+// }
+//
+// alert(result);
+
+
+const temp = +prompt('Введите температуру:');
+const type = prompt('Перевести в градусы Цельсия - C, c или Фаренгейты - F, f?');
+let result;
+
+if (!isNaN(temp)) {
+    if (type.toLowerCase() === 'f') {
+        result = 'Результат: ' + ((temp - 32) * 5/9) + '°C';
+    } else if (type.toLowerCase() === 'c') {
+        result = 'Результат: ' + ((temp * 9/5) + 32) + '°F';
+    } else {
+        result = 'Ошибка! Введите "C" или "F" для указания типа температуры.';
+    }
+} else {
+    result = 'Ошибка! Введите число вначале.';
+}
+
+alert(result);
+
