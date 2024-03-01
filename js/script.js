45d74bdd1e9047a0f508c2cbf58aee9cce3ed3d0
le-fonite <lefonite@gmail.com>
2024-03-01 13:27:31 +0200

    hw_10 completed

--- js/script.js
+++ js/script.js
@@ -1,2 +1,20 @@
 'use strict';
 
+const firstOperand = prompt('Введите первое число: ');
+const secondOperand = prompt('Введите второе число и получите результат: ');
+
+// #1 вариант решения:
+// const resultPlus = (+firstOperand + +secondOperand);
+// or
+const resultPlus = parseFloat(firstOperand) + parseFloat(secondOperand);
+const resultMinus = (firstOperand - secondOperand);
+const resultMultiply = (firstOperand * secondOperand);
+const resultDivision = (firstOperand / secondOperand);
+
+console.log(`
+        Результаты:
+        - Сложение: ${firstOperand} + ${secondOperand} = ${resultPlus}
+        - Вычитание: ${firstOperand} - ${secondOperand} = ${resultMinus}
+        - Умножение: ${firstOperand} * ${secondOperand} = ${resultMultiply}
+        - Деление: ${firstOperand} / ${secondOperand} = ${resultDivision}
+    `);
