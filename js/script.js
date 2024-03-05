b41de5f1f7d9b1ed9d273b44416a4daedcb23a19
le-fonite <lefonite@gmail.com>
2024-03-05 22:15:32 +0200

    hw_14 completed

--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
 'use strict';
 
+const num1 = +prompt('Введите первое число:');
+const num2 = +prompt('Введите второе число и получите результат:');
+
+const resultPlus = num1 + num2;
+const resultMinus = num1 - num2;
+const resultMultiply = num1 * num2;
+const resultDivision = num1 / num2;
+
+alert(`Ваш результат:\n
+  ${num1} + ${num2} = ${resultPlus}\n
+  ${num1} - ${num2} = ${resultMinus}\n
+  ${num1} * ${num2} = ${resultMultiply}\n
+  ${num1} / ${num2} = ${resultDivision}`);
