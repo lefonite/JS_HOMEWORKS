1e31ee73be9f95952d4668a94f8f6de8eb67377b
le-fonite <lefonite@gmail.com>
2024-03-09 23:29:15 +0200

    hw_21 completed

--- js/script.js
+++ js/script.js
@@ -1,101 +1,94 @@
-'use strict';
-let a = prompt('Введите число для a: ');
-let  b = prompt('Введите число для b: ');
-const num = +prompt('Введите число от 1-4: ');
+const a = prompt('Введите значение для "a":');
+const b = prompt('Введите значение для "b":');
+const num = parseInt(prompt('Введите целое число от 1 до 4:'));
 
-// Задание 1 (Якщо змінна a дорівнює нулю, виведіть 'Вірно', інакше виведіть 'Неправильно'.):
-if (parseInt(a) === 0) {
+let result;
+
+// Задача 1
+if (parseFloat(a) === 0) {
     console.log('Верно');
 } else {
     console.log('Неверно');
 }
 
-// Задание 2 (a більша за нуль, то виведіть 'Вірно'):
-if (a > 0) {
+// Задача 2
+if (parseFloat(a) > 0) {
     console.log('Верно');
 } else {
     console.log('Неверно');
 }
 
-// Задание 3 (a менша за нуль):
-if (a < 0) {
+// Задача 3
+if (parseFloat(a) < 0) {
     console.log('Верно');
 } else {
     console.log('Неверно');
 }
 
-// Задание 4 (a більша або дорівнює нулю):
-if (a >= 0) {
-    console.log('Верно');
-} else {
-    console.log('Неправильно');
-}
-
-// Задание 5 (a менша або дорівнює нулю):
-if (a <= 0) {
+// Задача 4
+if (parseFloat(a) >= 0) {
     console.log('Верно');
 } else {
     console.log('Неверно');
 }
 
-// Задание 6 (a не дорівнює нулю):
-if (parseInt(a) !== 0) {
+// Задача 5
+if (parseFloat(a) <= 0) {
     console.log('Верно');
 } else {
     console.log('Неверно');
 }
 
-// Задание 7 (змінна a дорівнює 'test'):
+// Задача 6
+if (parseFloat(a) !== 0) {
+    console.log('Верно');
+} else {
+    console.log('Неверно');
+}
+
+// Задача 7
 if (a === 'test') {
     console.log('Верно');
 } else {
     console.log('Неверно');
 }
 
-// Задание 8
-if (parseInt(a) === 1) {
+// Задача 8
+if (a === '1') {
     console.log('Верно');
 } else {
     console.log('Неверно');
 }
 
-// Задание 9
-if (a > 0 && a < 5) {
+// Задача 9
+if (parseFloat(a) > 0 && parseFloat(a) < 5) {
     console.log('Верно');
 } else {
     console.log('Неверно');
 }
 
-// Задание 10
-let result10;
-if (parseInt(a) === 0 || parseInt(a) === 2) {
-    result10 = +a + 7;
+// Задача 10
+if (parseFloat(a) === 0 || parseFloat(a) === 2) {
+    console.log(parseFloat(a) + 7);
 } else {
-    result10 = a /= 10;
+    console.log(parseFloat(a) / 10);
 }
-console.log(result10);
 
-
-// Задание 11
-let result11;
-if (parseInt(a) <= 1 && parseInt(b) >= 3) {
-    result11 = a + b;
+// Задача 11
+if (parseFloat(a) <= 1 && parseFloat(b) >= 3) {
+    console.log(parseFloat(a) + parseFloat(b));
 } else {
-    result11 = a - b;
+    console.log(parseFloat(a) - parseFloat(b));
 }
-console.log(result11);
 
-
-
-// Задание 12
-if ((a > 2 && a < 11) || (b >= 6 && b <= 14)) {
+// Задача 12
+if ((parseFloat(a) > 2 && parseFloat(a) < 11) || (parseFloat(b) >= 6 && parseFloat(b) <= 14)) {
     console.log('Верно');
 } else {
     console.log('Неверно');
 }
 
-// Задание 13
-let result;
+// Задача 13
 switch (num) {
     case 1:
         result = 'зима';
@@ -113,5 +106,3 @@ switch (num) {
         result = 'нет такого';
 }
 console.log(result);
-
-
