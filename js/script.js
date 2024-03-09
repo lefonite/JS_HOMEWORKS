394e651026db82800817e97b6fc80825e3de52c0
le-fonite <lefonite@gmail.com>
2024-03-09 16:04:17 +0200

    hw_19 completed

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
 'use strict';
 
+const userNumber = prompt('Введите свой возраст или год рождения: ');
+const lastDigit = userNumber % 10;
+const lastTwoDigits = userNumber % 100;
+
+if (isNaN(userNumber)){
+    alert('Пишите цифрами возраст!');
+} else {
+    if ((lastTwoDigits >= 11 && lastTwoDigits <= 19) || (lastDigit === 0) || (lastDigit >= 5 && lastDigit <= 9)) {
+        alert(userNumber + " лет");
+    } else if ((lastDigit === 1) || (userNumber >= 1000 && userNumber <= 9999)) {
+        alert(userNumber + " год");
+    } else {
+        alert(userNumber + " года");
+    }
+}
+
+
