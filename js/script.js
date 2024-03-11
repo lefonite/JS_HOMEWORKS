91e0a00096e25ff53ddc542c0e4c5931adbfef53
le-fonite <lefonite@gmail.com>
2024-03-11 12:39:22 +0200

    hw_18 completed

--- js/script.js
+++ js/script.js
@@ -1,13 +1,33 @@
 'use strict';
 
+// let numOrStr = prompt('input number or string');
+// console.log(numOrStr)
+// if(numOrStr === null) {
+//     console.log('ви скасували')
+// } else if( numOrStr.trim() === '' ) {
+//     console.log('Empty String');
+// } else if ( isNaN( +numOrStr ) ) {
+//     console.log(' number is Ba_NaN')
+// } else {
+//     console.log('OK!')
+// }
+
+
 let numOrStr = prompt('input number or string');
-console.log(numOrStr)
-if(numOrStr === null) {
-    console.log('ви скасували')
-} else if( numOrStr.trim() === '' ) {
-    console.log('Empty String');
-} else if ( isNaN( +numOrStr ) ) {
-    console.log(' number is Ba_NaN')
-} else {
-    console.log('OK!')
+console.log(numOrStr);
+
+switch (true) {
+    case (numOrStr === null):
+        console.log('ви скасували');
+        break;
+    case (numOrStr.trim() === ''):
+        console.log('Empty String');
+        break;
+    case (isNaN(+numOrStr)):
+        console.log('number is Ba_NaN');
+        break;
+    default:
+        console.log('OK!');
+        break;
 }
+
