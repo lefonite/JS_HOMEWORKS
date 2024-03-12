5579bcfb575c12f447cb349c6f2624092ea5be2c
le-fonite <lefonite@gmail.com>
2024-03-12 23:38:18 +0200

    hw_26 completed

--- js/script.js
+++ js/script.js
@@ -2,13 +2,14 @@
 
 let sum = 0;
 
-for (let i = 1; i <= 20; i++) {
+outerLoop: for (let i = 1; i <= 20; i++) {
     if (i % 2 === 0) {
-        break;
+        continue outerLoop;
     }
     sum += i;
 }
 
 console.log('Сумма нечетных чисел от 1 до 20: ' + sum);
+
 
 
