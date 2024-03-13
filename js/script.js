79751c9d8a2623cea8944a44bdad7c5fb2e98867
le-fonite <lefonite@gmail.com>
2024-03-13 13:41:14 +0200

    hw_24 completed

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
 'use strict';
 
+
+const arr = [1, 2, 3, 4, 5];
+let sum = 0;
+let sumOfSquares = 0;
+for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+}
+for (let i = 0; i < arr.length; i++) {
+    sumOfSquares += Math.pow(arr[i], 2);
+}
+console.log('Сумма элементов массива: ',sum);
+console.log('Сумма квадратов элементов массива: ', sumOfSquares);
+
+
+
+
