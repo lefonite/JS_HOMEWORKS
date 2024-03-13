ddc83f6316438a9157bf9a12b3ae5c3ec898d68a
le-fonite <lefonite@gmail.com>
2024-03-13 12:07:31 +0200

    hw_25 completed

--- js/script.js
+++ js/script.js
@@ -1,2 +1,21 @@
 'use strict';
 
+const userNumber = +prompt('Введите число и получите минимальный делитель: ');
+
+if (isNaN(userNumber) || userNumber < 1) {
+    console.log('NaN');
+} else {
+    let smallestDivisor = 2;
+
+    while (smallestDivisor <= userNumber) {
+        if (userNumber % smallestDivisor === 0) {
+            console.log(`Наименьший делитель числа ${userNumber}: ${smallestDivisor}`);
+            break;
+        }
+        smallestDivisor++;
+    }
+
+    if (smallestDivisor > userNumber) {
+        console.log(`Число ${userNumber} простое`);
+    }
+}
