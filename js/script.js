f041b78e6a495a42e0c42fa343cd991a6ef72026
le-fonite <lefonite@gmail.com>
2024-03-15 01:05:17 +0200

    hw_22 completed

--- js/script.js
+++ js/script.js
@@ -1,106 +1,102 @@
 'use strict';
 
 // 1.
-console.log('числа від 10 до 20: ');
+let result1 = '';
 for (let i = 10; i <= 20; i++) {
-    console.log(i);
+    result1 += i + ', ';
 }
+document.write('● 1 - Вивести на сторінку в один рядок через кому числа від 10 до 20: ' + result1);
+
 
 // 2.
-console.log('квадрати чисел від 10 до 20: ');
+document.write('<br> ● 2 - Квадрати чисел від 10 до 20: ');
 for (let i = 10; i <= 20; i++) {
-    console.log(Math.pow(i, 2));
+    const square = Math.pow(i, 2);
+    document.write(square + ', ');
 }
 
 // 3.
-console.log('Вивести таблицю множення на 7: ');
+document.write('<br> ● 3 - Вивести таблицю множення на 7: ');
 for (let i = 1; i <= 10; i++) {
-    console.log(7 * i);
+    document.write(` ${7 * i}`+ ', ');
 }
 
 // 4.
-console.log('сума всіх цілих чисел від 1 до 15: ');
-let sum = 0;
+let sum4 = 0;
 for (let i = 1; i <= 15; i++) {
-    sum+=i;
-    console.log(sum);
+    sum4+=i;
 }
+document.write('<br> ● 4 - Сума всіх цілих чисел від 1 до 15: ' + sum4 );
 
-// 4.
-console.log('добуток усіх цілих чисел від 15 до 35: ');
+// 5.
 let product = 1;
 for (let i = 15; i <= 35; i++) {
-    console.log(product *= i);
+    product *= i;
 }
-
+document.write('<br> ● 5 - Добуток усіх цілих чисел від 15 до 35: ' + product);
 
 // 6.
-let sum2 = 0;
-let count = 0;
+let sum6 = 0;
 for (let i = 1; i <= 500; i++) {
-    sum2 += i;
-    count++;
+    sum6 += i;
 }
-const average = sum2 / count;
-console.log('середнє арифметичне всіх цілих чисел від 1 до 500: ', average);
+document.write('<br> ● 6 - середнє арифметичне всіх цілих чисел від 1 до 500: ' + sum6 / 500);
+
+
 
 
 // 7.
-let sum3 = 0;
+let sum7 = 0;
 for (let i = 30; i <= 80; i++) {
     if (i % 2 === 0) {
-        sum3 += i;
+        sum7 += i;
     }
 }
-console.log('Сума парних чисел від 30 до 80:', sum3);
+document.write('<br> ● 7 - Сума парних чисел від 30 до 80: ' + sum7);
 
 
 // 8.
-console.log('всі числа в діапазоні від 100 до 200 кратні 3.: ');
+let result8 = '';
 for (let i = 100; i <= 200; i++) {
     if (i % 3 === 0) {
-        console.log(i);
+        result8 += i + ', ';
     }
 }
+document.write('<br> ● 8 - Всі числа в діапазоні від 100 до 200 кратні 3: ' + result8);
 
 
 // 9.
 // 10.
 // 11.
-console.log('Дано натуральне число. Знайти та вивести на сторінку всі його дільники. Визначити кількість його парних дільників. Знайти суму його парних дільників.: ');
-const naturalNumber = prompt('Введіть натуральне число:');
-const number = parseInt(naturalNumber);
+const number = 24;
+document.write(`<br> ● 9 - Дільники числа ${number}: `);
 
-if (isNaN(number) || number <= 0) {
-    console.log('Будь ласка, введіть коректне натуральне число.');
-} else {
-    console.log(`Дільники числа ${number}: `);
+let evenDivisorsCount = 0;
+let evenDivisorsSum = 0;
 
-    let evenDivisorsCount = 0;
-    let evenDivisorsSum = 0;
+for (let i = 1; i <= number; i++) {
+    if (number % i === 0) {
+        document.write(i + ', ');
 
-    for (let i = 1; i <= number; i++) {9,
-        if (number % i === 0) {
-            console.log(i);
-
-            if (i % 2 === 0) {
-                evenDivisorsCount++;
-                evenDivisorsSum += i;
-            }
+        if (i % 2 === 0) {
+            evenDivisorsCount++;
+            evenDivisorsSum += i;
         }
     }
-
-    console.log(`Кількість парних дільників: ${evenDivisorsCount}`);
-    console.log(`Сума парних дільників: ${evenDivisorsSum}`);
 }
+
+document.write(`<br> ● 10 - Кількість парних дільників: ${evenDivisorsCount}`);
+document.write(`<br> ● 11 - Сума парних дільників: ${evenDivisorsSum}`);
+
+
 
 
 
 
 // 12.
-console.log('повна таблицю множення від 1 до 10.: ');
 for (let i = 1; i <= 10; i++) {
     for (let j = 1; j <= 10; j++) {
         console.log(`${i} * ${j} = ${i * j}`);
     }
 }
+document.write('<br> ● 12 - Повна таблицю множення від 1 до 10: ' + result8);
