9a0c503155d2f5de528cddb242d8605c29b86c53
le-fonite <lefonite@gmail.com>
2024-03-16 13:26:50 +0200

    hw_28 completed

--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
 'use strict';
 
+const userNumber = prompt('Введите число:');
+const exponent = prompt('Введите степень:');
+
+function power(base, exp) {
+    exp = exp || 1;
+    if (isNaN(base) || isNaN(exp)) {
+        return 'Ошибка! Вводите число';
+    }
+    const result =   Math.pow(base, exp);
+    return result;
+}
+const result = power(userNumber, exponent);
+alert(result);
