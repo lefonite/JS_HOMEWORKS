419d8503197f14a0f93d787732631f5c2b0c6949
le-fonite <lefonite@gmail.com>
2024-03-17 02:23:33 +0200

    hw_29 completed

--- js/script.js
+++ js/script.js
@@ -1,2 +1,26 @@
 'use strict';
 
+const padString = function (str, len, sym = '*', fromLeft = true){
+    if (typeof str !== 'string') return 'Ошибка! Первый аргумент должен быть строкой.';
+    if (typeof len !== 'number' || len < 0) return 'Ошибка! Второй аргумент должен быть положительным числом.';
+    if (typeof sym !== 'string' || sym.length !== 1 ) return 'Ошибка! Третий аргумент должен быть строкой длиной в 1 символ.';
+    if (typeof fromLeft !== 'boolean') return 'Ошибка! Четвертый аргумент должен быть булевым значением.';
+
+    if (str.length >= len) {
+        return str.substring(0, len);
+    }
+
+    const diff = len - str.length;
+    const padding = sym.repeat(diff);
+    return fromLeft ? str + padding : padding + str;
+}
+console.log (padString('hello', 8,));
+console.log (padString('hello', 6, '*', false));
+console.log (padString('hello', 2,));
+console.log(padString(123, 8)); // Выведет 'Ошибка! Первый аргумент должен быть строкой.'
+console.log(padString('hello', -8)); // Выведет 'Ошибка! Второй аргумент должен быть положительным числом.'
+console.log(padString('hello', '8')); // Выведет 'Ошибка! Второй аргумент должен быть положительным числом.'
+console.log(padString('hello', 8, '**')); // Выведет 'Ошибка! Третий аргумент должен быть строкой длиной в 1 символ.'
+console.log(padString('hello', 8, '*', 'true')); // Выведет 'Ошибка! Четвертый аргумент должен быть булевым значением.'
+
+
