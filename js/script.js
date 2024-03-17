312f3e5950d5f32c68fc32f76cf209c25c0e452f
le-fonite <lefonite@gmail.com>
2024-03-17 02:46:17 +0200

    hw_30 init

--- js/script.js
+++ js/script.js
@@ -1,2 +1,25 @@
 'use strict';
 
+function filterPositiveNumbers(arr) {
+    const positiveNumbers = [];
+    if (arr.length === 0) {
+        return 'Массив пустой';
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > 0) {
+            positiveNumbers.push(arr[i]);
+        }
+    }
+
+    if (positiveNumbers.length > 0) {
+        return positiveNumbers;
+    } else {
+        return null;
+    }
+}
+
+
+const arr = [1, 2, 3, -1, -2, -3];
+console.log(filterPositiveNumbers(arr)); // Выведет [1, 2, 3]
+
