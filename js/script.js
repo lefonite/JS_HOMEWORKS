ff5041b2378409845034bba4a33adcd41ba23e7b
le-fonite <lefonite@gmail.com>
2024-03-24 01:17:41 +0200

    hw_31 init

--- js/script.js
+++ js/script.js
@@ -1,2 +1,59 @@
 'use strict';
 
+// 1. Объединить массивы
+const arr = [1, 2, 3];
+const arr1 = [4, 5, 6];
+console.log(arr.concat(arr1));
+
+// 2. Отзеркалить массив
+const arr2 = [1, 2, 3];
+arr2.reverse();
+console.log(arr2);
+
+// 3. Добавить в конец элементы
+const arr3 = [1, 2, 3];
+arr3.push(4, 5, 6);
+console.log(arr3);
+
+// 4. Добавить в начало элементы
+const arr4 = [1, 2, 3];
+arr4.unshift(4, 5, 6);
+console.log(arr4);
+
+// 5. Выведите на экран первый элемент
+const arr5 = ['js', 'css', 'jq'];
+console.log(arr5[0]);
+
+// 6. Выведите на экран первый элемент
+console.log(arr5[arr5.length - 1]);
+
+// 7. Запишите в новый массив элементы из начала
+const arr7 = [1, 2, 3, 4, 5];
+const newArr7 = arr7.slice(0,3);
+console.log(newArr7);
+
+// 8. Запишите в новый массив элементы с конца
+const newArr8 = arr7.slice(3,5);
+// or
+// const newArr8 = arr7.slice(-2);
+console.log(newArr8);
+
+// 9. преобразуйте массив в [1, 4, 5]
+const newArr9 = arr7.slice(0, 1).concat(arr7.slice(3));
+console.log(newArr9);
+
+// 10. запишите в новый массив элементы [2, 3, 4].
+const newArr10 = arr7.slice(1, 4);
+console.log(newArr10);
+
+// 11. сделайте из него массив [1, 2, 3, 'a', 'b', 'c', 4, 5].
+arr7.splice(3, 0, 'a', 'b', 'c');
+console.log(arr7);
+
+// 12. сделайте из него массив [1, 'a', 'b', 2, 3, 4, 'c', 5, 'e'].
+const arr12 = [1, 2, 3, 4, 5];
+arr12.splice(1, 0, 'a', 'b');
+arr12.splice(6, 0, 'c');
+arr12.push('e');
+console.log(arr12);
+
