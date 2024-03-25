ccf58160eff5b42b7b6261643a85971f6bed118b
le-fonite <lefonite@gmail.com>
2024-03-25 02:27:58 +0200

    hw_32 completed

--- js/script.js
+++ js/script.js
@@ -93,7 +93,7 @@ const array = [[1, 2, 3], [4, 5], [6]];
 const totalSum = sumArray(array);
 console.log(totalSum);
 
-// 11. Найдите сумму элементов этого массива. Массив, конечно же, может быть произвольным.
+// 11. Найдите сумму элементов этого массива.
 function sumArray11(arr) {
     let sum = 0;
     for (let i = 0; i < arr.length; i++) {
